31bc56d6f830f2fa43ef2e6cce0deaf690bdffb3
Saeyoon Lee <ericalee97@naver.com>
2022-03-23 13:52:53 +0900

    Update weather-observation-station-4.sql

--- BasicSelect/weather-observation-station-4.sql
+++ BasicSelect/weather-observation-station-4.sql
@@ -1,5 +1,4 @@
--- https://www.hackerrank.com/challenges/weather-observation-station-4/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+-- https://www.hackerrank.com/challenges/weather-observation-station-4/problem?isFullScreen=true
 
 SELECT COUNT(*) - COUNT(DISTINCT CITY)
 FROM STATION
-WHERE LAT_N > 0 AND LONG_W > 0
