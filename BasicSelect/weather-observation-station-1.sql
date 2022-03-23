bfff76753f9c64ad735d54ed5268d8ebeb3e6d80
Saeyoon Lee <ericalee97@naver.com>
2022-03-23 13:52:19 +0900

    Update weather-observation-station-1.sql

--- BasicSelect/weather-observation-station-1.sql
+++ BasicSelect/weather-observation-station-1.sql
@@ -1,5 +1,4 @@
--- https://www.hackerrank.com/challenges/weather-observation-station-1/problem?isFullScreen=true&h_r=next-challenge&h_v=zen
+-- https://www.hackerrank.com/challenges/weather-observation-station-1/problem?isFullScreen=true
 
 SELECT CITY, STATE
 FROM STATION
-WHERE LAT_N > 0 AND LONG_W > 0
