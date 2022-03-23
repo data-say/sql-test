337e26452c7e875f33b14d9f8e70e1282463b04d
Saeyoon Lee <ericalee97@naver.com>
2022-03-23 13:52:35 +0900

    Update weather-observation-station-3.sql

--- BasicSelect/weather-observation-station-3.sql
+++ BasicSelect/weather-observation-station-3.sql
@@ -1,6 +1,5 @@
--- https://www.hackerrank.com/challenges/weather-observation-station-3/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+-- https://www.hackerrank.com/challenges/weather-observation-station-3/problem?isFullScreen=true
 
 SELECT DISTINCT CITY
 FROM STATION
 WHERE ID % 2 = 0
-    AND LAT_N > 0 AND LONG_W > 0
